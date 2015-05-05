15fb86ff331f086ffcc05d6a386ce1df7a468a1d
fc01 <910677221@qq.com>
2015-05-05 15:08:39 +0800

    _

---
+++ X_TableView.h
@@ -0,0 +1,52 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+
+#import "NSMutableArray+x.h"
+
+typedef NS_ENUM(NSInteger, X_TableViewEvent) {
+    XTableViewDidScroll,
+    XTableViewWillReloadData,
+    XTableViewDidReloadData,
+};
+
+@class X_TableView;
+
+@protocol X_TableViewEffect<NSObject>
+-(void)onEffectAdd:(X_TableView *)xTableView;
+-(void)onEffectRemove:(X_TableView *)xTableView;
+@end
+
+@interface X_TableView : UITableView
+
+//dataSource and delegate are disabled
+//it's not weak
+//@property(nonatomic,strong)id<X_TableViewDataSource> xDataSource;
+@property(nonatomic,strong)NSMutableArray *xDataSource;
+
+//tableView event
+//instead of KVO
+-(void)addTableEventListenerWithId:(id)Id name:(enum X_TableViewEvent)name block:(void(^)())block;
+-(void)removeTableEventWithId:(id)Id;
+
+//cell event
+-(void)addCellEventListenerWithName:(NSString*)name block:(void (^)(NSMutableDictionary* cellData))block;
+-(void)dispatchCellEventWithName:(NSString *)name data:(NSMutableDictionary*)data;
+-(void)removeAllCellEvent;
+
+//effect
+-(void)addEffect:(id<X_TableViewEffect>)effect;
+-(void)removeEffect:(id<X_TableViewEffect>)effect;
+
+@end
+
+
+@interface X_TableViewCell : UITableViewCell
+@property(nonatomic,weak) X_TableView *xTableView;
+@property(nonatomic) NSMutableDictionary *cellData;
+-(void)initialize;
+-(void)update;
+-(void)didSelect;
+-(CGFloat)getCellHeight;
+-(void)dispatchCellEventWithName:(NSString *)name;
+@end
--- X_TableView.podspec
+++ X_TableView.podspec
@@ -7,7 +7,7 @@ Pod::Spec.new do |s|
   s.author       = { 'fc01' => '910677221@qq.com' }
   s.source       = { :git => "https://github.com/fc01/X_TableView.git", :tag => s.version.to_s }
   s.platform     = :ios, '7.0'
-  s.source_files = 'X_TableView/*.{h,m}'
+  s.source_files = '*.{h,m}'
   s.framework    = "UIKit"
   s.requires_arc = true
 end
